11c6cd20a4bac3d04c4a43e038601cd603e7cbbe
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-14 06:21:55 +0200

    fix: Explicitly set std lib versions

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { parse as parseYaml } from "https://deno.land/std/encoding/yaml.ts";
-export { red } from "https://deno.land/std/fmt/colors.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.42.0/encoding/yaml.ts";
+export { red } from "https://deno.land/std@0.50.0/fmt/colors.ts";
 export { cac } from "https://raw.githubusercontent.com/BentoumiTech/cac/fix/window-pathname-not-exist/mod.js";
