9ad3a051cab13bc6e7f64837f800afa69e93bde7
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-14 16:42:22 +0200

    feat: Add YAMLError, assertThrows and resolve dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 export { parse as parseYaml } from "https://deno.land/std@0.42.0/encoding/yaml.ts";
+export { YAMLError } from "https://deno.land/std@0.42.0/encoding/yaml/error.ts";
+
 export { red } from "https://deno.land/std@0.50.0/fmt/colors.ts";
 
 // @deno-types="https://unpkg.com/cac@v6.5.9/mod.d.ts"
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,4 +1,7 @@
 export {
   assertEquals,
   assertArrayContains,
+  assertThrows
 } from "https://deno.land/std@0.50.0/testing/asserts.ts";
+
+export { resolve } from "https://deno.land/std@0.50.0/path/mod.ts";
