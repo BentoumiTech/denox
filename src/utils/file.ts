75cb60bcc25dc79d0c4c0a84e388d0e9ac65ee05
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-14 15:40:52 +0200

    feat: Add a method that given a list of file return the content of the first existing file

---
+++ src/utils/file.ts
@@ -0,0 +1,21 @@
+import { WorkspaceNotFoundError } from "./DenoErrors.ts";
+
+function readFirstExistingFile(files: string[]): string {
+  const [firstFile, ...restFiles] = files;
+  try {
+    const fileBytes = Deno.readFileSync(firstFile);
+    const decoder = new TextDecoder("utf-8");
+    return decoder.decode(fileBytes);
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      if (restFiles.length > 0) {
+        return readFirstExistingFile(restFiles);
+      }
+      throw new WorkspaceNotFoundError();
+    }
+
+    throw e;
+  }
+}
+
+export { readFirstExistingFile };
