f7a09a40fae933592ccb971df618b9b3b9171a94
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-14 18:32:33 +0200

    test: Import assertStrContains

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,6 +2,7 @@ export {
   assertEquals,
   assertArrayContains,
   assertThrows,
+  assertStrContains
 } from "https://deno.land/std@0.50.0/testing/asserts.ts";
 
 export { resolve } from "https://deno.land/std@0.50.0/path/mod.ts";
