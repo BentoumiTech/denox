d35aabc66e7f06b8f9adbe10d4a564f11a513bbe
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-15 06:25:43 +0200

    feat: Use .yml extension as default for workspace file

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 const CURRENT_VERSION = "0.2.0";
 const GITHUB_REPO_NAME = "BentoumiTech/denox";
 const DENO_WORKSPACE_FILES = [
-  "deno-workspace",
   "deno-workspace.yml",
+  "deno-workspace",
   "deno-workspace.yaml",
   ".deno-workspace",
   ".deno-workspace.yml",
