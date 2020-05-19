bcdabfa52e4f073c3cda6aee2fb878e67fae70a4
Khaled Bentoumi <kbentoumi@gmail.com>
2020-05-19 08:40:51 +0200

    feat: Add extname dep and assertThrowsAsync

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,8 +1,11 @@
+export * from './deps.ts';
+
 export {
   assertEquals,
   assertArrayContains,
   assertThrows,
   assertStrContains,
+  assertThrowsAsync
 } from "https://deno.land/std@0.50.0/testing/asserts.ts";
 
-export { resolve, join } from "https://deno.land/std@0.50.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.50.0/path/mod.ts";
