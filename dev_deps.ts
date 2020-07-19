730bab2a52ddd1da6f9db37ce080b8a584b5a632
Roy Ivy III <rivy.dev@gmail.com>
2020-07-19 13:14:01 -0500

    Fix (windows) path URL type compiler error

--- dev_deps.ts
+++ dev_deps.ts
@@ -8,4 +8,4 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.50.0/testing/asserts.ts";
 
-export { join } from "https://deno.land/std@0.50.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.61.0/path/mod.ts";
