a4e5035a048068d0bc039ce5a731ba609ee26ce0
Khaled Bentoumi <kbentoumi@gmail.com>
2020-08-12 15:37:00 +0700

    Downgrade std

--- dev_deps.ts
+++ dev_deps.ts
@@ -6,8 +6,8 @@ export {
   assertThrows,
   assertStringContains,
   assertThrowsAsync,
-} from "https://deno.land/std@0.64.0/testing/asserts.ts";
+} from "https://deno.land/std@0.60.0/testing/asserts.ts";
 
-export { stripColor } from "https://deno.land/std@0.64.0/fmt/colors.ts";
+export { stripColor } from "https://deno.land/std@0.60.0/fmt/colors.ts";
 
-export { join } from "https://deno.land/std@0.64.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.60.0/path/mod.ts";
